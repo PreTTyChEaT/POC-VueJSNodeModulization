b68beb56fcf8fc1baef2bad180879635bae859da
PreTTyChEaT <46685716+PreTTyChEaT@users.noreply.github.com>
2020-12-30 04:07:43 +0800

    POC with node modulization tutorial 01 and 02
    https://itnext.io/create-a-vue-js-component-library-as-a-module-part-1-a1116e632751
    
    https://itnext.io/create-a-vue-js-component-library-part-2-c92a42af84e9
    
    https://cli.vuejs.org/guide/build-targets.html#async-web-component

---
+++ sampleapp/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <dummy-button />
+  </div>
+</template>
+<script>
+//import DummyButton from 'dummylib'
+export default {
+  name: 'app'
+}
+</script>
---
+++ sampleapp/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import store from './stores/store.js';
+import DummyButton from 'dummylib';
+
+Vue.use(DummyButton, { store });
+Vue.config.productionTip = false
+new Vue({
+  store,
+  render: h => h(App),
+}).$mount('#app')
